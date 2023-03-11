00d4011ffd11d230c22f309a655301f2a4c4b09e
AnujMongia <122998528+AnujMongia@users.noreply.github.com>
2023-03-10 19:34:55 -0500

    Added 6 more test cases

--- UnitTest_CSCN71020_A2/UnitTest_CSCN71020_A2.cpp
+++ UnitTest_CSCN71020_A2/UnitTest_CSCN71020_A2.cpp
@@ -1,8 +1,12 @@
 #include "pch.h"
 #include "CppUnitTest.h"
 
+
 extern "C" int getPerimeter(int*, int*);
 extern "C" int getArea(int*, int*);
+extern "C" void setLength(int, int*);
+extern "C" void setWidth(int, int*);
+
 
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
@@ -34,5 +38,66 @@ namespace UnitTestCSCN71020A2
 			int actual_area = getArea(&length, &width);
 			Assert::AreEqual(expected_area, actual_area);
 		}
+
+		TEST_METHOD(setLengthTestCase1)
+		{
+			
+			int length;
+			int new_length = 71;
+			setLength(new_length, &length);
+			Assert::AreEqual(new_length, length);
+
+		}
+
+		TEST_METHOD(setLengthTestCase2)
+		{
+
+			int length;
+			int new_length = 192;
+			setLength(new_length, &length);
+			Assert::AreEqual(new_length, length);
+
+		}
+
+		TEST_METHOD(setLengthTestCase3)
+		{
+
+			int length;
+			int new_length = 0;
+			setLength(new_length, &length);
+			Assert::AreEqual(new_length, length);
+
+		}
+
+		TEST_METHOD(setWidthTestCase1)
+		{
+
+			int width;
+			int new_width = 41;
+			setWidth(new_width, &width);
+			Assert::AreEqual(new_width, width);
+
+		}
+
+		TEST_METHOD(setWidthTestCase2)
+		{
+
+			int width;
+			int new_width = 173;
+			setWidth(new_width, &width);
+			Assert::AreEqual(new_width, width);
+
+		}
+
+		TEST_METHOD(setWidthTestCase3)
+		{
+
+			int width;
+			int new_width = 100;
+			setWidth(new_width, &width);
+			Assert::AreEqual(new_width, width);
+
+		}
+
 	};
 }
